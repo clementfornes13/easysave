ef16940072b6f1dcc578ba27f5fb7fd8b6866cda
thomas noe <thomasnoe13@hotmail.fr>
2023-02-01 11:37:09 +0100

    Initial Commit with SaveFile basic class

---
+++ EasySave/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasySave
+{
+    class EasySave
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class SaveFile
+    {
+        private string m_name;
+        private string m_pathFrom, m_pathTo;
+        private uint sizeFile;
+        private uint transfertTime;
+
+        SaveFile(string filename)
+        {
+            m_name = filename;
+        }
+        SaveFile(string filename, string pathFrom, string pathTo) 
+        {
+            m_name = filename;
+            m_pathFrom = pathFrom;
+            m_pathTo = pathTo;
+        }
+        ~SaveFile()
+        {
+
+        }
+
+        public bool calcTransfertTime()
+        {
+            //Calc time to transfert from => to
+            return true;
+        }
+
+        public string Name { get => m_name; }
+        public string PathFrom { get => m_pathFrom; set => m_pathFrom = value; }
+        public string PathTo { get => m_pathTo; set => m_pathTo = value; }
+        public uint SizeFile { get => sizeFile; set => sizeFile = value; }
+        public uint TransfertTime { get => transfertTime; }
+    }
+}
