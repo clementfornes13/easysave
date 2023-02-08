9b0a71bc96499f1f7975b4c79a1ff02ce1dff1d1
Langlet_Jordan <jordan.langlet@viacesi.fr>
2023-02-08 12:27:47 +0100

    Optimisation LogsFile

--- EasySave/LogsFile.cs
+++ EasySave/LogsFile.cs
@@ -1,118 +1,92 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.IO;
 
 namespace EasySave
 {
-
-    /*class EasySave
-     {
-         static void Main(string[] args)
-         {
-            
+    // Class for tests
+    class EasySave
+    {
+        static void Main(string[] args)
+        {
             LogsFile mylog = new LogsFile();
         }
-
-     }*/
+    }
 
     class LogsFile
     {
-        //Variable 
-        private static StreamReader m_readingStream;
-        private static StreamWriter m_writinggStream;
-
-        public static StreamReader ReadingStream { get => m_readingStream; set => m_readingStream = value; }
-        public static StreamWriter WritingStream { get => m_writinggStream; set => m_writinggStream = value; }
-        
-        private string LogsFilePath = System.Environment.CurrentDirectory + @"\Logs\";
+        // Variable definitions
+        private string logsFilePath = System.Environment.CurrentDirectory + @"\Logs\";
+        private string logsFileName = "Log {0}.csv";
+        private StreamWriter writingStream;
 
         public LogsFile()
         {
             string name = "name 2";
-            string pathfrom = "from 2";
-            string pathto = "to 2";
-            string sizefile = "sizefile 2";
-            string transfertTime = "transfertTime 2";
+            string pathFrom = "from 2";
+            string pathTo = "to 2";
+            string sizeFile = "sizefile 2";
+            string transferTime = "transfertTime 2";
 
-            WriteLog(name, pathfrom, pathto, sizefile, transfertTime);
+            WriteLog(name, pathFrom, pathTo, sizeFile, transferTime);
         }
 
-        bool CreateLogFile()
+        // Create a Logs folder
+        private void CreateDirectory()
         {
-            // Formatting the date
-            DateTime currentDate = DateTime.Now;
-            string formattedDate = currentDate.ToString("dd-MM-yyyy");
-
-            if (!Directory.Exists(LogsFilePath)) //Check if the folder is created
+            // Check if the folder already exists
+            if (!Directory.Exists(logsFilePath))
             {
-                DirectoryInfo di = Directory.CreateDirectory(LogsFilePath); //Function that creates the folder
-            }
-
-            string filePath = System.Environment.CurrentDirectory + @"\Logs\Log " + formattedDate + ".csv";
-
-            try
-            {
-                // Create an empty file
-                if (!File.Exists(filePath))
-                {
-                    File.Create(filePath).Close();
-                    WritingStream = new StreamWriter(filePath, true);
-                    WritingStream.WriteLine("Timestamp;Name;Source path;Destination path;File size;Transfer time");
-                    WritingStream.Close();
-                    return true;
-                }
-                else
-                {
-                    return true;
-                }
-            }
-            catch (Exception e)
-            {
-                System.Console.Error.WriteLine(e.ToString());
-                return false;
+                Directory.CreateDirectory(logsFilePath);
             }
         }
 
-        bool WriteLog(string name, string pathfrom, string pathto, string sizefile, string transfertTime)
+        private void CreateFile()
+        {
+            CreateDirectory();
+
+            // Define the path to take and the name of the log file
+            string formattedDate = DateTime.Now.ToString("dd-MM-yyyy");
+            string filePath = Path.Combine(logsFilePath, string.Format(logsFileName, formattedDate));
+
+            // Check if the file already exists
+            if (!File.Exists(filePath))
+            {
+                using (writingStream = new StreamWriter(filePath, false))
+                {
+                    // Write the header in the csv file
+                    writingStream.WriteLine("Timestamp;Name;Source path;Destination path;File size;Transfer time");
+                }
+            }
+        }
+
+        public bool WriteLog(string name, string pathFrom, string pathTo, string sizeFile, string transferTime)
         {
             try
             {
+                CreateFile();
 
-                if (CreateLogFile() == true)
+                // Define the path to take 
+                using (writingStream = new StreamWriter(Path.Combine(logsFilePath, string.Format(logsFileName, DateTime.Now.ToString("dd-MM-yyyy"))), true))
                 {
-                    // Formatting the date
-                    DateTime currentDate = DateTime.Now;
-                    string formattedDate = currentDate.ToString("dd-MM-yyyy");
-
-
-
-                    string filePath = System.Environment.CurrentDirectory + @"\Logs\Log " + formattedDate + ".csv";
-
-
-                    // Open the write stream to write to the file
-                    WritingStream = new StreamWriter(filePath, true);
-
-                    // Write log entries
-                    WritingStream.WriteLine(DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss.f") + ";" + name + ";" + pathfrom + ";" + pathto + ";" + sizefile + ";" + transfertTime);
-
-                    // Close the write stream
-                    WritingStream.Close();
-
+                    // Write the various information to the file
+                    writingStream.WriteLine(
+                        "{0};{1};{2};{3};{4};{5}",
+                        DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss.f"),
+                        name,
+                        pathFrom,
+                        pathTo,
+                        sizeFile,
+                        transferTime
+                    );
                 }
+
                 return true;
             }
             catch (Exception e)
             {
-                System.Console.Error.WriteLine(e.ToString());
+                Console.Error.WriteLine(e);
                 return false;
             }
         }
-
     }
-
-
-
 }
