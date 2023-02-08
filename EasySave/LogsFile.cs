47447595821dc41b231a0beec62be9fdbdcac686
Langlet_Jordan <jordan.langlet@viacesi.fr>
2023-02-08 16:09:24 +0100

    Modif avec JSON LogsFile

--- EasySave/LogsFile.cs
+++ EasySave/LogsFile.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Newtonsoft.Json;
 using System.IO;
 
 namespace EasySave
@@ -8,8 +9,10 @@ namespace EasySave
     {
         // Variable definitions
         private string logsFilePath = System.Environment.CurrentDirectory + @"\Logs\";
-        private string logsFileName = "Log {0}.csv";
-        private StreamWriter writingStream;
+
+        // A NE PAS ENLEVER
+        //private string logsFileName = "Log {0}.csv";
+        // private StreamWriter writingStream;
 
         // Create a Logs folder
         private void CreateDirectory()
@@ -21,45 +24,32 @@ namespace EasySave
             }
         }
 
-        private void CreateFile()
-        {
-            CreateDirectory();
-
-            // Define the path to take and the name of the log file
-            string formattedDate = DateTime.Now.ToString("dd-MM-yyyy");
-            string filePath = Path.Combine(logsFilePath, string.Format(logsFileName, formattedDate));
-
-            // Check if the file already exists
-            if (!File.Exists(filePath))
-            {
-                using (writingStream = new StreamWriter(filePath, false))
-                {
-                    // Write the header in the csv file
-                    writingStream.WriteLine("Timestamp;Name;Source path;Destination path;File size;Transfer time");
-                }
-            }
-        }
-
-        public bool WriteLog(string name, string pathFrom, string pathTo, string sizeFile, string transferTime)
+        public bool WriteLogJson(string name, string pathFrom, string pathTo, string sizeFile, string transferTime)
         {
             try
             {
-                CreateFile();
+                CreateDirectory();
 
                 // Define the path to take 
-                using (writingStream = new StreamWriter(Path.Combine(logsFilePath, string.Format(logsFileName, DateTime.Now.ToString("dd-MM-yyyy"))), true))
+                string formattedDate = DateTime.Now.ToString("dd-MM-yyyy");
+                string filePath = Path.Combine(logsFilePath, string.Format("Log {0}.json", formattedDate));
+
+                // Create a log object with the provided information
+                var log = new
                 {
-                    // Write the various information to the file
-                    writingStream.WriteLine(
-                        "{0};{1};{2};{3};{4};{5}",
-                        DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss.f"),
-                        name,
-                        pathFrom,
-                        pathTo,
-                        sizeFile,
-                        transferTime
-                    );
-                }
+                    Time = DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss.f"),
+                    Name = name,
+                    SourcePath = pathFrom,
+                    DestinationPath = pathTo,
+                    SizeFile = sizeFile,
+                    TransferTime = transferTime
+                };
+
+                // Serialize the log object to JSON format
+                string logJson = JsonConvert.SerializeObject(log, Formatting.Indented);
+
+                // Write the JSON string to the file
+                File.AppendAllText(filePath, logJson);
 
                 return true;
             }
@@ -69,5 +59,61 @@ namespace EasySave
                 return false;
             }
         }
+
+        //////////////////////////////////////////////////////////////////////
+        //Création d'un fichier .Csv à garder pour les prochaines versions
+        //////////////////////////////////////////////////////////////////////
+        ///
+        /* private void CreateFile()
+         {
+             CreateDirectory();
+
+             // Define the path to take and the name of the log file
+             string formattedDate = DateTime.Now.ToString("dd-MM-yyyy");
+             string filePath = Path.Combine(logsFilePath, string.Format(logsFileName, formattedDate));
+
+             // Check if the file already exists
+             if (!File.Exists(filePath))
+             {
+                 using (writingStream = new StreamWriter(filePath, false))
+                 {
+                     // Write the header in the csv file
+                     writingStream.WriteLine("Time;Name;Source path;Destination path;File size;Transfer time");
+                 }
+             }*/
+
+        //////////////////////////////////////////////////////////////////////
+        // Ecriture pour un fichier .Csv à garder pour les prochaines versions
+        //////////////////////////////////////////////////////////////////////
+        ///
+        /* public bool WriteLog(string name, string pathFrom, string pathTo, string sizeFile, string transferTime)
+         {
+             try
+             {
+                 CreateFile();
+
+                 // Define the path to take 
+                 using (writingStream = new StreamWriter(Path.Combine(logsFilePath, string.Format(logsFileName, DateTime.Now.ToString("dd-MM-yyyy"))), true))
+                 {
+                     // Write the various information to the file
+                     writingStream.WriteLine(
+                         "{0};{1};{2};{3};{4};{5}",
+                         DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss.f"),
+                         name,
+                         pathFrom,
+                         pathTo,
+                         sizeFile,
+                         transferTime
+                     );
+                 }
+
+                 return true;
+             }
+             catch (Exception e)
+             {
+                 Console.Error.WriteLine(e);
+                 return false;
+             }
+         }*/
     }
 }
