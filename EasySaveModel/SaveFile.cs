0789bd9a4f38603cfe340af1e60563ad0f5771fc
thomas noe <thomasnoe13@hotmail.fr>
2023-02-17 15:14:21 +0100

    Change var name to respect CS standard

--- EasySaveModel/SaveFile.cs
+++ EasySaveModel/SaveFile.cs
@@ -7,23 +7,23 @@ namespace EasySaveModel
 {
     public class SaveFiles
     {
-        private List<FileInfo> m_files = new List<FileInfo>();
-        private List<DirectoryInfo> subDirs = new List<DirectoryInfo>();
-        private string m_pathFrom, m_pathTo; //C:/dir/dir/dir
-        private long totalSizeFile = 0;
+        private List<FileInfo> _files = new List<FileInfo>();
+        private List<DirectoryInfo> _subDirs = new List<DirectoryInfo>();
+        private string _pathFrom, _pathTo; //C:/dir/dir/dir
+        private long _totalSizeFile = 0;
 
         public SaveFiles(string pathFrom, string pathTo)
         {
-            m_pathFrom = pathFrom;
+            _pathFrom = pathFrom;
             if (pathTo == null || pathTo == "" || pathTo == "Destination")
             {
-                m_pathTo = System.Environment.CurrentDirectory
+                _pathTo = System.Environment.CurrentDirectory
                     + @"\Backups\"
                     + System.Threading.Thread.CurrentThread.ManagedThreadId;
             }
             else
             {
-                m_pathTo = pathTo;
+                _pathTo = pathTo;
             }
             init();
             calcSizeFiles();
@@ -32,19 +32,19 @@ namespace EasySaveModel
         private void init()
         {
             //Need to make a feature for subdirectory
-            string[] names = System.IO.Directory.GetFiles(m_pathFrom);
+            string[] names = System.IO.Directory.GetFiles(_pathFrom);
             if (names.Length == 0)
             {
-                throw new DirectoryNotFoundException("DirectoryError" + m_pathFrom);
+                throw new DirectoryNotFoundException("DirectoryError" + _pathFrom);
             }
 
             foreach (string filename in names)
             {
-                m_files.Add(new FileInfo(filename));
+                _files.Add(new FileInfo(filename));
             }
 
-            DirectoryInfo dirFrom = new DirectoryInfo(m_pathFrom);
-            subDirs = new List<DirectoryInfo>(dirFrom.GetDirectories());
+            DirectoryInfo dirFrom = new DirectoryInfo(_pathFrom);
+            _subDirs = new List<DirectoryInfo>(dirFrom.GetDirectories());
         }
         ~SaveFiles()
         {
@@ -52,37 +52,37 @@ namespace EasySaveModel
         }
         public void calcSizeFiles()
         {
-            foreach (FileInfo file in m_files)
+            foreach (FileInfo file in _files)
             {
-                totalSizeFile += file.Length;
+                _totalSizeFile += file.Length;
             }
-            foreach (DirectoryInfo dir in subDirs)
+            foreach (DirectoryInfo dir in _subDirs)
             {
                 FileInfo[] subFiles = dir.GetFiles();
                 foreach (FileInfo file in subFiles)
                 {
-                    totalSizeFile += file.Length;
+                    _totalSizeFile += file.Length;
                 }
             }
             Logging();
         }
         public void Logging()
         {
-            string m_nameLog = System.IO.Path.GetFileName(m_pathFrom);
-            string totalSizeFileLog = totalSizeFile.ToString();
+            string m_nameLog = Path.GetFileName(_pathFrom);
+            string totalSizeFileLog = _totalSizeFile.ToString();
             string transferTime = "0";
 
             LogsFile JSONmyLogs = LogsFile.GetInstance(true);
             LogsFile XMLmyLogs = LogsFile.GetInstance(true);
 
-            JSONmyLogs.WriteLog(m_nameLog, m_pathFrom, m_pathTo, totalSizeFileLog, transferTime);
-            XMLmyLogs.WriteLog(m_nameLog, m_pathFrom, m_pathTo, totalSizeFileLog, transferTime);
+            JSONmyLogs.WriteLog(m_nameLog, _pathFrom, _pathTo, totalSizeFileLog, transferTime);
+            XMLmyLogs.WriteLog(m_nameLog, _pathFrom, _pathTo, totalSizeFileLog, transferTime);
         }
 
-        public List<FileInfo> Files { get => m_files; }
-        public List<DirectoryInfo> SubDirs { get => subDirs; }
-        public string PathFrom { get => m_pathFrom; set => m_pathFrom = value; }
-        public string PathTo { get => m_pathTo; set => m_pathTo = value; }
-        public long TotalSizeFile { get => totalSizeFile; }
+        public List<FileInfo> Files { get => _files; }
+        public List<DirectoryInfo> SubDirs { get => _subDirs; }
+        public string PathFrom { get => _pathFrom; set => _pathFrom = value; }
+        public string PathTo { get => _pathTo; set => _pathTo = value; }
+        public long TotalSizeFile { get => _totalSizeFile; }
     }
 }
--- EasySaveModel/TransfertStates.cs
+++ EasySaveModel/TransfertStates.cs
@@ -1,23 +1,29 @@
 ﻿using System;
 using System.Diagnostics;
-using System.Resources;
 using System.IO;
 
 namespace EasySaveModel
 {
     public class TransfertStatesItems
     {
-        private SaveFiles m_files;
-        private bool m_actualStates = false; //Active/Waiting
-        private double m_elapsedTransfertTime;
-        private int m_nbFiles, m_nbFilesMoved;
+        private SaveFiles _files;
+        private bool _actualStates = false; //Active/Waiting
+        private double _elapsedTransfertTime;
+        private int _nbFiles, _nbFilesMoved;
 
         public TransfertStatesItems(SaveFiles files)
         {
-            m_files = files;
-            m_nbFiles = files.Files.Count;
+            _files = files;
+            _nbFiles = files.Files.Count;
         }
 
+        //Threading backup
+        //new mutex
+        //if complete
+        //if sequentiel
+        //New the
+
+        
         //Make a fill copy
         public void BackUp()
         {
@@ -27,16 +33,16 @@ namespace EasySaveModel
             stopwatch.Start(); //Starting the timed for the log file
 
             //Create main directory
-            if (!Directory.Exists(m_files.PathTo))
+            if (!Directory.Exists(_files.PathTo))
             {
-                Directory.CreateDirectory(m_files.PathTo);
+                Directory.CreateDirectory(_files.PathTo);
             }
 
             //Move Files
-            m_actualStates = true;
-            foreach (FileInfo file in m_files.Files)
+            _actualStates = true;
+            foreach (FileInfo file in _files.Files)
             {
-                string targetFile = Path.Combine(m_files.PathTo, file.Name);
+                string targetFile = Path.Combine(_files.PathTo, file.Name);
 
                 try
                 {
@@ -47,15 +53,15 @@ namespace EasySaveModel
                 }
                 catch (Exception e) { Console.Error.Write(e.ToString()); }
 
-                m_elapsedTransfertTime = stopwatch.Elapsed.TotalSeconds;
-                m_nbFilesMoved++;
+                _elapsedTransfertTime = stopwatch.Elapsed.TotalSeconds;
+                _nbFilesMoved++;
 
             }
 
             //Manage sub dir for copy
-            foreach (DirectoryInfo dir in m_files.SubDirs)
+            foreach (DirectoryInfo dir in _files.SubDirs)
             {
-                string targetdir = Path.Combine(m_files.PathTo, dir.Name);
+                string targetdir = Path.Combine(_files.PathTo, dir.Name);
                 if (!Directory.Exists(targetdir))
                 {
                     Directory.CreateDirectory(targetdir);
@@ -76,33 +82,35 @@ namespace EasySaveModel
                     }
                     catch (Exception e) { Console.Error.Write(e.ToString()); }
 
-                    m_elapsedTransfertTime = stopwatch.Elapsed.TotalSeconds;
-                    m_nbFilesMoved++;
+                    _elapsedTransfertTime = stopwatch.Elapsed.TotalSeconds;
+                    _nbFilesMoved++;
 
                 }
             }
             stopwatch.Stop();
-            m_actualStates = false;
+            _actualStates = false;
+            //prendre Mutex
             Loggin();
+            //Rendre mutex
         }
 
         public void Loggin()
         {
-            string m_nameLog = Path.GetFileName(m_files.PathFrom);
-            string totalSizeFileLog = m_files.TotalSizeFile.ToString();
-            string m_elapsedTransfertTimeLog = m_elapsedTransfertTime.ToString();
+            string m_nameLog = Path.GetFileName(_files.PathFrom);
+            string totalSizeFileLog = _files.TotalSizeFile.ToString();
+            string m_elapsedTransfertTimeLog = _elapsedTransfertTime.ToString();
             
             LogsFile JSONmyLogs = LogsFile.GetInstance(true);
             LogsFile XMLmyLogs = LogsFile.GetInstance(true);
 
-            JSONmyLogs.WriteLog(m_nameLog, m_files.PathFrom, m_files.PathTo, totalSizeFileLog, m_elapsedTransfertTimeLog);
-            XMLmyLogs.WriteLog(m_nameLog, m_files.PathFrom, m_files.PathTo, totalSizeFileLog, m_elapsedTransfertTimeLog);
+            JSONmyLogs.WriteLog(m_nameLog, _files.PathFrom, _files.PathTo, totalSizeFileLog, m_elapsedTransfertTimeLog);
+            XMLmyLogs.WriteLog(m_nameLog, _files.PathFrom, _files.PathTo, totalSizeFileLog, m_elapsedTransfertTimeLog);
         }
 
-        public bool ActualStates { get => m_actualStates; set => m_actualStates = value; }
-        public double ElapsedTransfertTime { get => m_elapsedTransfertTime; }
-        internal SaveFiles workingFile { get => m_files; }
-        public int NbFiles { get => m_nbFiles; set => m_nbFiles = value; }
-        public int NbFilesMoved { get => m_nbFilesMoved; set => m_nbFilesMoved = value; }
+        public bool ActualStates { get => _actualStates; set => _actualStates = value; }
+        public double ElapsedTransfertTime { get => _elapsedTransfertTime; }
+        internal SaveFiles workingFile { get => _files; }
+        public int NbFiles { get => _nbFiles; set => _nbFiles = value; }
+        public int NbFilesMoved { get => _nbFilesMoved; set => _nbFilesMoved = value; }
     }
 }
