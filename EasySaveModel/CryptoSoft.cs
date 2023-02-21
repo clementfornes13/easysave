7e1e5332bef5327a5b789234ba8346a09ffada35
R-Florent <florent.real@viacesi.fr>
2023-02-21 13:55:25 +0100

    ajout du processus qui permet d'appeler l'exe CryptoSoft

--- EasySaveModel/CryptoSoft.cs
+++ EasySaveModel/CryptoSoft.cs
@@ -1,9 +1,18 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace EasySaveModel
 {
     public class CryptoSoft
     {
+        Process process1 = new Process();
+        process1.StartInfo.FileName = "explorer.exe";
+        process1.Start();
 
     }
 }
