461c0f57c392942cdf5e08904a50fc8ed3b473a3
R-Florent <florent.real@viacesi.fr>
2023-02-22 10:50:13 +0100

    classe appelant cryptosoft

---
+++ EasySaveModel/CryptoSoft.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasySaveModel
+{
+    public class CryptoSoft
+    {
+<<<<<<< HEAD
+        Process processCryptoSoft = new Process();
+        processCryptoSoft.StartInfo.FileName = "..\CryptoSoft\CryptoSoft\bin\Debug\net6.0\";
+        processCryptoSoft.Start();
+=======
+
+>>>>>>> 4f605c137671ea71902dfe5abab513536b76e2eb
+    }
+}
